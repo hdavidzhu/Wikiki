4f195717fc9c0ead6fc81870d5721908ff8a4c24
Chris <sihrc.c.lee@gmail.com>
2015-02-28 16:49:49 -0500

    FIX: Edit UI

--- public/jsx/components/RepoPage.jsx
+++ public/jsx/components/RepoPage.jsx
@@ -3,63 +3,62 @@ var RepoPage = React.createClass({
         return (
             <div>
                 <h1> @{this.props.owner}/{this.props.name}</h1>
-                <RepoContent name={this.props.name} />
+                <ReadMe name={this.props.name} />
             </div>
             );
     }
 });
 
-var RepoContent = React.createClass({
+var ReadMe = React.createClass({
     render: function () {
         github.README(this.props.name, function (data) {
             React.render(
-                (<div>
-                    <EditButton data = {data} content="github_readme"/>
-                    <div id="github_readme_content"> {data} </div>
+                (<div id="readme_contentBox">
+                    <EditButton data = {data} content="readme"/>
+                    <div> {data} </div>
                 </div>)
-                , document.getElementById('github_readme'));
+                , document.getElementById('readme'));
         });
 
-        return (<div id="github_readme"></div>);
+        return (<div id="readme"></div>);
+    }
+});
+
+var EditButton = React.createClass({
+    whenClicked: function () {
+        removeElement(this.props.content + "_contentBox");
+
+        React.render(
+            <div id={this.props.content + "_editContentBox"}>
+                <EditContent content={this.props.content} data={this.props.data} />
+            </div>
+            , document.getElementById(this.props.content));
+    },
+    render: function () {
+        return (
+            <button onClick = {this.whenClicked}> Edit </button>
+        );
     }
 });
 
 var EditContent = React.createClass({
     render: function ()  {
         return (
-            <div id="editContentForm">
-                <SaveButton content="contentBox" />
-                <textarea id="contentBox"> {this.props.data} </textarea>
+            <div>
+                <SaveButton content={this.props.content}/>
+                <textarea defaultValue={this.props.data}/>
             </div>
-        );
-    }
-});
-
-var EditButton = React.createClass({
-    whenClicked: function () {
-        removeElement(this.props.content + "_editContentForm");
-
-        React.render((
-            <div id={this.props.content + "_contentBox"}>
-                <SaveButton name={this.props.content + "_edit"} />
-                <EditContent name={this.props.content} data={this.props.data} />
-            </div>)
-            , document.getElementById(this.props.content));
-    },
-    render: function () {
-        return (
-            <button id={this.props.content + "_editButton"} onClick = {this.whenClicked}> Edit </button>
         );
     }
 });
 
 var SaveButton = React.createClass({
     whenClicked: function () {
-        removeElement(this.props.content + "_editContentForm");
+        removeElement(this.props.content + "_editContentBox");
     },
     render: function () {
         return (
-            <button id="saveButton" onClick = {this.whenClicked}> Save </button>
+            <button onClick = {this.whenClicked}> Save </button>
         );
     }
 });
