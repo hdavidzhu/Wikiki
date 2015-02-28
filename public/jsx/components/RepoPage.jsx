8e840e69ae57c200b58cedb2dc0d5602438ed881
Chris <sihrc.c.lee@gmail.com>
2015-02-28 15:23:17 -0500

    ADD: Edit Readme UI Implementation

--- public/jsx/components/RepoPage.jsx
+++ public/jsx/components/RepoPage.jsx
@@ -14,13 +14,58 @@ var RepoContent = React.createClass({
         github.README(this.props.name, function (data) {
             React.render(
                 (<div>
-                    {data}
+                    <EditButton data = {data} content="github_readme"/>
+                    <div id="github_readme_content"> {data} </div>
                 </div>)
                 , document.getElementById('github_readme'));
         });
 
-        return (<div>
-            <div id="github_readme"> </div>
-            </div>);
+        return (<div id="github_readme"></div>);
     }
 });
+
+var EditContent = React.createClass({
+    render: function ()  {
+        return (
+            <div id="editContentForm">
+                <SaveButton content="contentBox" />
+                <textarea id="contentBox"> {this.props.data} </textarea>
+            <div>
+        );
+    }
+});
+
+var EditButton = React.createClass({
+    OnClick: function () {
+        removeElement({this.props.content + "_contentBox"});
+
+        React.render(
+            <div id={this.props.content + "_contentBox">
+                <SaveButton name={this.props.content + "_edit"}/>
+                <EditContent name={this.props.content} data={this.props.data} />
+            </div>
+            , document.getElementById(this.props.content));
+    },
+    render: function () {
+        return (
+            <button id={this.props.content + "_editButton"} onClick = {this.OnClick}> Edit </button>
+        );
+    }
+});
+
+var SaveButton = React.createClass({
+    OnClick: function () {
+        removeElement({this.props.content + "_editContentForm"});
+    },
+    render: function () {
+        return (
+            <button id="saveButton" onClick = {this.OnClick}> Save </button>
+        );
+    }
+});
+
+
+var removeElement = function (id) {
+    var elem = document.getElementById(id);
+    elem.parentNode.removeChild(elem);
+}
